e36f945fedc08881a00429b3dd4c328e899008c7
Alejandro <terremoto_alejandro@hotmail.com>
2026-04-30 16:11:53 -0400

    ruta del servidor

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const app = express();
 // --- MIDDLEWARES ---
     app.use(express.json()); 
     app.use(express.urlencoded({ extended: true }));
-    app.use(express.static(path.join(__dirname, 'views'))); 
-    app.use(express.static(path.join(__dirname, 'assets'))); 
+    app.use(express.static(path.join(__dirname, '..', 'views'))); 
+    app.use(express.static(path.join(__dirname, '..','assets'))); 
 
 // --- CONFIGURACIÓN DE SESIÓN ---
     app.use(session({
