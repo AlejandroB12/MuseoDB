9a6abe056026d1080475267a661a8395393a8b3e
Kelvys Concepcion ✨ <kelvys.10cena@gmail.com>
2026-04-30 15:23:44 -0400

    Changes to paths and files names

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
     app.use(express.json()); 
     app.use(express.urlencoded({ extended: true }));
     app.use(express.static(path.join(__dirname, 'views'))); 
-    app.use(express.static(path.join(__dirname, 'Assets'))); 
+    app.use(express.static(path.join(__dirname, 'assets'))); 
 
 // --- CONFIGURACIÓN DE SESIÓN ---
     app.use(session({
